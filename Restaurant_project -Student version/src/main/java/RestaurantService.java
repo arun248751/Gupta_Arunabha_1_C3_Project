cfdc457b8ea90cc9191769d8744f7a888b53e155
arun248751 <arunabhagupta03@gmail.com>
2021-09-29 22:17:46 +0530

    Part 2: Solution

--- Restaurant_project -Student version/src/main/java/RestaurantService.java
+++ Restaurant_project -Student version/src/main/java/RestaurantService.java
@@ -5,12 +5,13 @@ import java.util.List;
 public class RestaurantService {
     private static List<Restaurant> restaurants = new ArrayList<>();
 
-    public Restaurant findRestaurantByName(String restaurantName){
-        return null;
-        //DELETE ABOVE STATEMENT AND WRITE CODE HERE
+
+
+    public Restaurant findRestaurantByName(String restaurantName) throws restaurantNotFoundException {
+        Optional<Restaurant> searchedRestaurantOpt = restaurants.stream().filter(restaurant -> restaurant.getName().equals(restaurantName)).findAny();
+        Restaurant searchedRestaurant = searchedRestaurantOpt.orElseThrow(() -> new restaurantNotFoundException("No Restaurant available with the name: " + restaurantName + "."));
+        return searchedRestaurant;
     }
-
-
     public Restaurant addRestaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
         Restaurant newRestaurant = new Restaurant(name, location, openingTime, closingTime);
         restaurants.add(newRestaurant);
